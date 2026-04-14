cc83e4b607ab051bbbf6cbd2db8a539234ba1265
Praveen <lunavathpraveen36@gmail.com>
2026-04-14 22:31:25 +0530

    deploy backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://smart-planner-evbn.onrender.com",
 });
 
 // ✅ Attach token to every request

ef94c8589284b2fa1be6c534416c5199d7ebd485
Praveen <lunavathpraveen36@gmail.com>
2026-04-14 23:29:41 +0530

    deploy backend

--- server/db.js
+++ server/db.js
@@ -1,24 +1,11 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,   // ✅ IMPORTANT FIX
-
-  ssl: {
-    rejectUnauthorized: false
-  },
-
-  waitForConnections: true,
-  connectionLimit: 10
-});
+const db = mysql.createConnection(process.env.DB_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("DB connection failed ❌:", err);
+    console.error("DB connection failed ❌", err);
   } else {
     console.log("MySQL Connected ✅");
   }
